cb4e7da2a8afcef80fbd62cc19e5942643057ee0
Luanna Silva <luanna.silva@bit.com.br>
2022-03-09 13:59:59 -0300

    Meu primeiro commit

---
+++ API.Cliente/Controllers/V1/ClienteController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Cliente.Modelo;
+
+
+namespace API.Cliente.Controllers.V1
+{
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private static List<ClienteModelo> Lista = new List<ClienteModelo>()
+        {
+            new ClienteModelo()
+            {
+               Nome = "Luanna",
+                Sobrenome = "Silva",
+                Cpf = "12500384640",
+                DataDeNascimento = new DateTime(1996,01,26)
+            },
+            new ClienteModelo()
+            {
+                Nome = "Adriana",
+                Sobrenome = "Alexandre",
+                Cpf = "85401650663",
+                DataDeNascimento = new DateTime(1972,03,18)
+            }
+        };
+
+        public ClienteController()
+        {
+            var cliente = new ClienteModelo();
+        }
+
+
+
+        // Buscar todos os clientes
+        [HttpGet]
+        [Route("api/v1/cliente")]
+        public ActionResult<List<ClienteModelo>> ObterTodosClientes()
+        {
+            if (Lista == null || Lista.Count == 0)
+            {
+                return NotFound("Nenhum cliente encontrado.");
+            }
+            return Ok(Lista);
+        }
+
+
+        //Buscar Clientes por cpf
+        [HttpGet("api/v1/cliente/{cpf}")]
+        public ActionResult<ClienteModelo> ObterCliente(string cpf)
+        {
+            var cliente = Lista.FirstOrDefault(cliente => cliente.Cpf  == cpf);
+            if (cliente == null ) 
+            {
+                return NotFound("Cliente não encontrado.");
+            }
+            return Ok(cliente);
+        }
+
+
+        [HttpPost]
+        [Route("api/v1/clientes")]
+
+        public ActionResult<string> CadastrarClientes([FromBody] ClienteModelo cliente)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                Lista.Add(cliente);
+                return Ok("Cliente cadastrado.");
+            }
+            catch
+            {
+                return BadRequest("Erro ao cadastrar cliente.");
+            }
+        }
+
+
+
+
+        [HttpPut]
+        [Route("api/v1/clientes{cpf}")]
+        public ActionResult<string> AlterarClientes(string cpf, [FromBody] ClienteModelo cliente)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                var BuscaCliente = Lista.FirstOrDefault(cliente => cliente.Cpf == cpf);
+                if (BuscaCliente != null)
+                {
+                    BuscaCliente.Nome = cliente.Nome;
+                    BuscaCliente.Sobrenome = cliente.Sobrenome;
+                }
+                return Ok("Atualizado com sucesso.");
+            }
+            catch
+            {
+                return NotFound("Cliente não encontrado.");
+            }
+            
+        }
+
+        [HttpDelete]
+        [Route("api/v1/clientes{cpf}")]
+        public ActionResult<string> DeletarClientes(string cpf)
+        {
+            var  cliente = Lista.FirstOrDefault(cliente => cliente.Cpf == cpf);
+            if (Lista.Remove(cliente!) == false)
+            {
+                return NotFound("Cliente não encontrado.");
+            }
+
+            return Ok("Cliente removido.");
+        }
+
+
+
+    }
+}
---
+++ API.Cliente/Modelo/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace API.Cliente.Modelo
+{
+    public class ClienteModelo
+    {
+        public string Nome { get; set; }  
+        [Required(ErrorMessage = "O nome é obrigatório!")]
+        public string Sobrenome { get; set; }
+        [Required(ErrorMessage = "O sobrenome é obrigatório!")]
+        public string Cpf { get; set; }
+        [Required(ErrorMessage = "O CPF é obrigatório!")]
+        public DateTime DataDeNascimento { get; set; }
+        
+
+
+        public ClienteModelo()
+        {
+        }
+        public ClienteModelo (string nome, string sobrenome, string cpf, DateTime dataDeNascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Cpf = cpf;
+            DataDeNascimento = dataDeNascimento;
+        }
+
+      
+
+
+
+    }
+}
